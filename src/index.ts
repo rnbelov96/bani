614e5f8af0b475ed3fa75235ff1ae43b59aa39f7
Belov Roman <rnbelov96@gmail.com>
2022-06-03 11:09:06 +0300

    Initiate the project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import './scss/style.scss';
+import './ts/year';
+import './ts/slider';
+import './ts/show-animations';
+// import './ts/modals';
+// import './ts/dropdowns';
+// import './ts/calc';
+// import './ts/setup-forms';
+// import './ts/f5leads-init';
+// import './ts/f5leads';
